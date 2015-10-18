2316b199abed07b7d72bea033706b30a8a7c09ba
nekikara <a.nek1427@gmail.com>
2015-10-18 23:36:20 +0900

    Couting

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import click
 
 @click.command()
-def cli():
-    """Example, script."""
-    click.echo("ken")
+@click.option('-v', '--verbose', count=True)
+def cli(verbose):
+    click.echo("Verbosity: %s" % verbose)
 
